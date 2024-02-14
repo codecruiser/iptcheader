cc8ad87d5f8e158013d9cb630de521fd81480ed9
Zbigniew Szczepański <zibby_pl@proton.me>
2024-02-14 20:06:35 +0100

    IPTC headers

--- src/tests/test_iptc_read.py
+++ src/tests/test_iptc_read.py
@@ -1,2 +1,14 @@
-def test_read_iptc():
-    pass
+import os
+
+from src.iptc_fiddler import IPTCFiddler
+
+
+def test_read_headers():
+    iptc_fiddler = IPTCFiddler()
+    print(os.path.abspath(__file__))
+    filename = 'photo-4iptc-heads.jpg'
+    headers = iptc_fiddler.read_headers(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), '..', 'images', filename
+    ))
+    print(headers)
+    assert 1==2
